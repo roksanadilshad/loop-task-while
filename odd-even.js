1127bbd1e9d71e3a11be7d2e7a58bfeb85208c25
ROKSANA DILSHAD <roksanadilshad@gmail.com>
2025-07-30 23:09:57 +0600

    odd-even.js

---
+++ odd-even.js
@@ -0,0 +1,18 @@
+//subtask-1
+/* let num = 61;
+
+while(num <= 100){
+    if(num % 2 !== 0){
+        console.log(num)
+    }
+    num++;
+} */
+//subtask-2
+let num2 = 78;
+
+while(num2 <= 98){
+    if(num2 % 2 === 0){
+        console.log(num2)
+    }
+    num2++;
+}
