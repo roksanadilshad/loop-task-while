4a11f90cc893f94c267c05c2961c3fcd2e04aeb2
ROKSANA DILSHAD <roksanadilshad@gmail.com>
2025-07-31 00:57:19 +0600

    hudai

--- commitment.js
+++ commitment.js
@@ -3,3 +3,9 @@ while(count <= 60){
     console.log("i will invest at least 6 hrs every single day for nest 60 days!");
     count++;
 }
+let i = 21;
+
+while(i >= 15){
+    console.log(i);
+    i--;
+}
